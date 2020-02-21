a5b2ee6b764b04ec6cb1a6964a88c02521e57ff0
Shaolin P <shiulan.poe@outlook.com>
2020-02-21 15:46:05 -0500

    update

--- Sort/Program.cs
+++ Sort/Program.cs
@@ -51,28 +51,28 @@ namespace Sort
         */
         static void Selection_Sort(int[] ss_arr)
         {
-            // min_position is used to keep track of the element position with the current lowest value
+                                                                    // min_position is used to keep track of the element position with the current lowest value
             int min_position;
-            // temp is used to conduct the swap during during the Selection Sort Algorithm
+                                                                    // temp is used to conduct the swap during during the Selection Sort Algorithm
             int temp;
 
             for (int i = 0; i < ss_arr.Length; i++)
             {
-                // Here we initialize the min_position to the current index of array
+                                                                    // Here we initialize the min_position to the current index of array
                 min_position = i;
-                // From the min_position, check to see if the next element is smaller
+                                                                    // From the min_position, check to see if the next element is smaller
                 for (int x = i + 1; x < ss_arr.Length; x++)
                 {
-                    // If the next element from the current min_position is smaller, then make that the new min_position
+                                                                    // If the next element from the current min_position is smaller, then make that the new min_position
                     if (ss_arr[x] < ss_arr[min_position])
                     {
-                        //min_position will keep track of the index that min is in, this is needed when a swap happens
+                                                                    //min_position will keep track of the index that min is in, this is needed when a swap happens
                         min_position = x;
                     }
-                } // End of inner for loop
+                }                                                   // End of inner for loop
 
-                // If the min_position does not equal the current element being evaluated in the loop
-                // Then execute the swap. by switching the postion of the lowest with the current element
+                                                                    // If the min_position does not equal the current element being evaluated in the loop
+                                                                    // Then execute the swap. by switching the postion of the lowest with the current element
                 if (min_position != i)
                 {
                     temp = ss_arr[i];
@@ -80,31 +80,31 @@ namespace Sort
                     ss_arr[min_position] = temp;
                 }
                 Console.Write("  " + ss_arr[i]);
-            } // End of outer for loop
-        } // End of Selection_Sort
+            }                                                       // End of outer for loop
+        }                                                           // End of Selection_Sort
 
-        // This method executes the Bubble Sort Algorithm with a worst case of time complexity (Big O Notation) of O(n^2)
+                                                                    // This method executes the Bubble Sort Algorithm with a worst case of time complexity (Big O Notation) of O(n^2)
         static void Bubble_Sort(int [] bs_arr)
         {
-            // The for loop will continue until the array is sorted in ascending order.
-            // Bubble Sort starts from the beginning of the array each for loop iteration and checks the value
-            // of the next element in the array to see if it is less. If it is less, then swap.
+                                                                    // The for loop will continue until the array is sorted in ascending order.
+                                                                    // Bubble Sort starts from the beginning of the array each for loop iteration and checks the value
+                                                                    // of the next element in the array to see if it is less. If it is less, then swap.
             for (int i = 0; i < bs_arr.Length - 1; i++)
             {
                 for (int x = 0; x < bs_arr.Length - i - 1; x++)                  
                 {
-                    // In each nested for loop iteration check to see if the next element value is lower than the current element value
-                    // If the next element value is less, then conduct the swap
+                                                                    // In each nested for loop iteration check to see if the next element value is lower than the current element value
+                                                                    // If the next element value is less, then conduct the swap
                     if (bs_arr[x] > bs_arr[x + 1])
                     {
-                        // Here you will swap temp and bs_arr[x]
-                        // The temp variable is used to temporarily hold the value of the current element so that it can be swapped
+                                                                    // Here you will swap temp and bs_arr[x]
+                                                                    // The temp variable is used to temporarily hold the value of the current element so that it can be swapped
                         int temp = bs_arr[x];
                         bs_arr[x] = bs_arr[x + 1];
                         bs_arr[x + 1] = temp;
-                    } // End of if
-                } // End of inner for loop
-            } // End of outer for loop
+                    }                                               // End of if
+                }                                                   // End of inner for loop
+            }                                                       // End of outer for loop
         }
     }
 }
